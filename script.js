6a0ae5a2ed92119c810769123a9e29239a196968
Tomaz_on_git <gumimixer@gmail.com>
2022-10-27 11:59:31 +0200

    fix minor issus (shades function, no border @ grid>50)

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
  let numberOfElements = 16*16 //default grid of 16x16 elements
+ 
  //let pixelColor = "bw"  //selector of three values, bw (default), rainbow, shadow, 
  document.getElementById("blackWhite").checked =true;
    createGrid(numberOfElements);  //just fix 16x16 for now
@@ -17,6 +18,10 @@ function createGrid(numberOfElements)
         const node = document.createElement("div");
         node.id = i;
         node.style.width = pixelWidth + "%";
+        node.style.backgroundColor = "rgb(255,255,255)"  //we sett all pixels to white
+        if (column > 50) {
+            node.style.borderStyle ="none";
+            } else {node.style.borderStyle ="solid";}
         document.getElementById("root").appendChild(node);
     }     
      // add listerens to grid and show the current position
@@ -26,7 +31,7 @@ function createGrid(numberOfElements)
     { 
         pixel.addEventListener('mouseenter', function (e) 
         { 
-        markPosition(e.target.id);
+            markPosition(e.target.id, e.target.style.backgroundColor);
         //console.log(e.target.id);
         })
     }
@@ -44,17 +49,17 @@ console.log(document.getElementById("root").Height);
 }
 */
 
-function markPosition(position) {
+function markPosition(position, Currentcolor) {
     document.getElementById("positionText").textContent = "Current div:" + position;
     //depanding on the selector selected 
     if (document.getElementById("blackWhite").checked) {
-    document.getElementById(position).style.backgroundColor = "black";
+    document.getElementById(position).style.backgroundColor = "rgb(0,0,0)";
     }
     if (document.getElementById("rainbow").checked) {
-        document.getElementById(position).style.backgroundColor = createColor(1);
+        document.getElementById(position).style.backgroundColor = createColor(1, Currentcolor);
     }
     if (document.getElementById("shades").checked) {
-        document.getElementById(position).style.backgroundColor = createColor(2);
+        document.getElementById(position).style.backgroundColor = createColor(2, Currentcolor);
     }
 
 }
@@ -75,18 +80,29 @@ function changeGrid(){
     }
 }
 
-function createColor(case2){
+function createColor(case2, currentColor){
     if (case2===1) {
     let colorString = "rgb(" + Math.floor((Math.random() * 255) + 1)+","
     colorString +=Math.floor((Math.random() * 255) + 1)+","
     colorString +=Math.floor((Math.random() * 255) + 1)+")"
     return colorString;
     };
-    if (case2===2) {
-        let colorString = "hsl( 0,0%,"
-        colorString +="90" 
-        colorString +="%)"
+    if (case2===2) {  //we are dealing with shades of gray - from 100% to as low as 0%
+        //console.log(currentColor);
+        let colorsAll = currentColor.slice(4,currentColor.length-1).split(",");    // let text = "rgb(230, 231, 232)"; text = text.slice(4,text.length-1);
+        console.log(colorsAll);
+        //make it darker
+        const colorsDarker = colorsAll.map(setColorDarker);
+        console.log(colorsDarker);
+
+        let colorString = "rgb(" + colorsDarker[0] +","
+        colorString +=colorsDarker[1] +"," 
+        colorString +=colorsDarker[0] +")"
         return colorString;
         };
 }
+
+function setColorDarker(value) {
+    return Math.abs((Number(value) - 25) % 255);
+  }
 //console.log(document.getElementById("blackWhite"));
